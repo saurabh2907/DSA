f37e5715d6f1dbad0b631292954363ff32a8681c
saurabh2907 <83023654+saurabh2907@users.noreply.github.com>
2023-06-25 11:45:10 +0530

    Create dsa1.py

---
+++ dsa1.py
@@ -0,0 +1,15 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.stack.pop()
+
+    def is_empty(self):
+        return len(self.stack) == 0
+      
