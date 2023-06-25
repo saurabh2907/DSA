392f2059b72a3afb2c9cabe068e15132aa637e5b
saurabh2907 <83023654+saurabh2907@users.noreply.github.com>
2023-06-25 11:46:19 +0530

    Create dsa2.py

---
+++ dsa2.py
@@ -0,0 +1,14 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        return self.queue.pop(0)
+
+    def is_empty(self):
+        return len(self.queue) == 0
